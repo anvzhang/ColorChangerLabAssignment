ef6adf4f2220798805aca3c3ac7ae5804bd9d469
anvzhang <165081556+anvzhang@users.noreply.github.com>
2024-04-17 20:33:27 +0000

    done

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function applyColor() {
-    const color = document.getElementById("color").value;
-    let backgroundColor;
+  const color = document.getElementById("color").value;
+  let backgroundColor;
 
-    // write your conditional if/else statements here:
+  // write your conditional if/else statements here:
+  if (color === "red") {
+    backgroundColor = "red";
+  } else if (color === "blue") {
+    backgroundColor = "blue";
+  } else if (color === "green") {
+    backgroundColor = "green";
+  } else if (color === "orange") {
+    backgroundColor = "orange";
+  } else if (color === "purple") {
+    backgroundColor = "purple";
+  } else if (color === "yellow") {
+    backgroundColor = "yellwo";
+  }
 
-
-    document.body.style.backgroundColor = backgroundColor;
+  document.body.style.backgroundColor = backgroundColor;
 }
